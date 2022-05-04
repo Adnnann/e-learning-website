e87e39a953e7c30b5aae834cab6a3be4e1645303
Adnan Ovcina <adnanovcina1987@gmail.com>
2022-05-04 23:19:50 +0200

    Init commit

---
+++ client/src/features/eLearningSlice.js
@@ -0,0 +1,12 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const initialState = {};
+
+const eLearningSlice = createSlice({
+  name: "eLearning",
+  initialState,
+  reducers: {},
+  extraReducers: {},
+});
+
+export default eLearningSlice.reducer;
