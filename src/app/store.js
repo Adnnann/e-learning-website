590a4c235393ab6a423fde9bbed11937b80e4921
Adnan Ovcina <adnanovcina1987@gmail.com>
2022-05-04 23:18:44 +0200

    Delete src directory

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import eLearningReducer from "../features/eLearningSlice";
-
-export const store = configureStore({
-  reducer: {
-    eLearning: eLearningReducer
-  }
-});
